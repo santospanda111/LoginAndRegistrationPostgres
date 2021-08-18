37c0d554370e0cdb8a8925e0610985c6fe627558
santospanda111 <santospanda111@gmail.com>
2021-08-18 20:47:11 +0530

    created LoginAndRegistration project

---
+++ LogInRegistration/LogInRegistration/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for LogInRegistration project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LogInRegistration.settings')
+
+application = get_wsgi_application()
