00d49560782d187154965c4f1b7e74428f8b1c92
santospanda111 <santospanda111@gmail.com>
2021-08-18 21:34:32 +0530

    Created LogInAPI to authenticate the user

--- LogInRegistration/UserRegistration/urls.py
+++ LogInRegistration/UserRegistration/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from UserRegistration import views as UserView
 
 urlpatterns = [
-    path('', UserView.Index.as_view(), name='homepage')
+    path('', UserView.Index.as_view(), name='homepage'),
+    path('/register', UserView.Register.as_view(), name='user_registration'),
+    path('register/<int:pk>/', UserView.Register.as_view(), name='user_registration'),
+    path('/login', UserView.LogIn.as_view(), name='user_login')
 ]
--- LogInRegistration/UserRegistration/views.py
+++ LogInRegistration/UserRegistration/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView,Response,status
 from django.contrib.auth.models import User
 from .serializer import UserSerializer
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError,AuthenticationFailed
+from django.contrib.auth import authenticate
 
 
 class Index(APIView):
@@ -12,6 +13,7 @@ class Index(APIView):
         return Response({'message':'Welcome to Login and Registration App'})
 
 class Register(APIView):
+    
     def get(self,request,pk=None):
         """
             This method is used to read the data from user_data.
@@ -51,3 +53,28 @@ class Register(APIView):
             return Response({'message': 'Invalid Input'}, status=status.HTTP_400_BAD_REQUEST)  
         except Exception as e:
             return Response({"msg": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogIn(APIView):
+   
+    def post(self,request):
+        """
+            This method is used for login authentication.
+            :param request: It's accept username and password as parameter.
+            :return: It returns the message if successfully loggedin.
+        """
+        try:
+            username = request.data['username']
+            password = request.data['password']
+            user = authenticate(username=username, password=password)            
+            if user is None:
+                return Response({"msg": 'Wrong username or password'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"msg": "Loggedin Successfully", 'data' : {'username': username}}, status=status.HTTP_200_OK)
+        except ValueError:
+            return Response({"message": 'Invalid Input'}, status=status.HTTP_400_BAD_REQUEST)
+        except ValidationError:
+            return Response({'message': "wrong credentials"}, status=status.HTTP_400_BAD_REQUEST) 
+        except AuthenticationFailed:
+            return Response({'message': 'Authentication Failed'}, status=status.HTTP_400_BAD_REQUEST) 
+        except Exception:
+            return Response({"msg": "wrong credentials"}, status=status.HTTP_400_BAD_REQUEST)
